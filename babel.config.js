5c05a7e0285b1a2046efc29be4444682b104ef11
BoyWithSilverWings <agney@outlook.in>
2019-01-16 23:53:41 +0530

    add babel config for env and react

---
+++ babel.config.js
@@ -0,0 +1,8 @@
+const presets = ["@babel/env", "@babel/preset-react"];
+
+const plugins = [
+  "@babel/plugin-proposal-object-rest-spread",
+  "@babel/plugin-proposal-class-properties",
+];
+
+module.exports = { presets, plugins };
